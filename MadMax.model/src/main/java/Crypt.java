24d62c7e35898cf5326df9550d0b5833f13b73c8
BlueVince <vincent.linck@viacesi.fr>
2019-09-24 14:05:21 +0200

    Class Crypt

---
+++ MadMax.model/src/main/java/Crypt.java
@@ -0,0 +1,22 @@
+
+public class Crypt {
+	
+	public String encrypt(String str, String key) {
+		String res = "";
+		char c1, c2, c3 = '0';
+		int i = 0;
+
+		for(i = 0; i < str.length(); i++) {
+
+			c1 = str.charAt(i);
+			c2 = key.charAt(i % key.length());
+			
+			c3 = (char)(c1 ^ c2);
+
+			res += c3;
+		}
+
+		return res;
+	}
+
+}
