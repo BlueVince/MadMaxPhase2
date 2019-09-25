d7df44e159dc10c68405a4c35b513a9edef208dc
pcmga <pcmga@10.54.128.136>
2019-09-25 14:01:08 +0200

    Push

--- MadMax.view/src/main/java/View/Fenetre.java
+++ MadMax.view/src/main/java/View/Fenetre.java
@@ -27,6 +27,7 @@ public class Fenetre extends JFrame implements ActionListener {
 	private JTextField tf1;
 	private JPasswordField p1;
 	private JButton btn1;
+	private JPanel panel;
 
 	/**
 	 * Launch the application.
@@ -56,63 +57,40 @@ public class Fenetre extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		
 		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(344, 104, 210, 23);
 		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblUsername.setForeground(new Color(65, 105, 225));
 		lblUsername.setBackground(Color.BLACK);
 		
 		tf1 = new JTextField();
+		tf1.setBounds(344, 133, 251, 31);
 		tf1.setColumns(10);
 		
 		p1 = new JPasswordField();
+		p1.setBounds(344, 202, 251, 32);
 		p1.setColumns(10);
 		
 		
 		
 		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(344, 170, 251, 26);
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblPassword.setForeground(new Color(65, 105, 225));
 		
 		this.setBtn1(new JButton("Se connecter"));
 		btn1.setForeground(Color.WHITE);
 		btn1.setBackground(new Color(65, 105, 225));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(387, Short.MAX_VALUE)
-					.addComponent(btn1, GroupLayout.PREFERRED_SIZE, 159, GroupLayout.PREFERRED_SIZE)
-					.addGap(54))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(339)
-					.addComponent(lblPassword, GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(339)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(tf1, GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
-						.addComponent(lblUsername, GroupLayout.PREFERRED_SIZE, 210, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(339, Short.MAX_VALUE)
-					.addComponent(p1, 251, 251, 251)
-					.addContainerGap())
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(99, Short.MAX_VALUE)
-					.addComponent(lblUsername, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(tf1, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(p1, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btn1, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-					.addGap(113))
-		);
-		contentPane.setLayout(gl_contentPane);
+		
+		panel = new JPanel();
+		panel.setBounds(73, 202, 112, 111);
+		panel.setLayout(null);
+		contentPane.setLayout(null);
+		contentPane.add(lblPassword);
+		contentPane.add(tf1);
+		contentPane.add(lblUsername);
+		contentPane.add(panel);
+		contentPane.add(btn1);
+		contentPane.add(p1);
 		
 		this.getBtn1().addActionListener(this);
 	}
@@ -139,6 +117,7 @@ public class Fenetre extends JFrame implements ActionListener {
 
 	public void setBtn1(JButton btn1) {
 		this.btn1 = btn1;
+		btn1.setBounds(392, 245, 159, 40);
 	}      
 	
 	public void actionPerformed(ActionEvent e) {
